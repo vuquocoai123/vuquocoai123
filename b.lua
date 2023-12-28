8f683d8476aa05893af1ec26b7986e03fa9b6039
vuquocoai123 <141901311+vuquocoai123@users.noreply.github.com>
2023-12-28 12:06:28 +0700

    Update b.lua

--- b.lua
+++ b.lua
@@ -15,7 +15,7 @@ getgenv().Setting = {
     ["Team"] = "Pirates", --Marines,Pirates
     ["Webhook"] = {
         ["Enabled"] = true,
-        ["Url Webhook"] = "", --Your Url
+        ["Url Webhook"] = "https://discord.com/api/webhooks/1189136716720783370/KMj8z0l5AoFSzkB-q1Y2XAWkWck4r7av-tndiTZrxbUxI0IsJO2bnTgBoEEBJmcXyDX2", --Your Url
     },
     ["Misc"] = {
         ["AutoBuyRandomandStoreFruit"] = false,
